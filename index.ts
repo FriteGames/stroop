26544c5953e8c56edd7e04017f252ac5720e3435
zachlite <zachlite@gmail.com>
2018-01-22 00:15:00 -0500

    drawing tiles

--- index.ts
+++ index.ts
@@ -3,45 +3,51 @@ import * as _ from 'lodash';
 
 let canvas;
 let ctx;
+let tiles;
 const SCREEN_HEIGHT = 720;
 const SCREEN_WIDTH = 1080;
 const BLOCK_SIZE = 32;
 
 function l(...args) {
-	console.log(...args);
+  console.log(...args);
 }
 
 function resizeCanvas() {
-	canvas.width = SCREEN_WIDTH;
-	canvas.height = SCREEN_HEIGHT;
+  canvas.width = SCREEN_WIDTH;
+  canvas.height = SCREEN_HEIGHT;
 }
 
 function init() {
-	canvas = document.getElementById('stroop') as HTMLCanvasElement;
-	ctx = canvas.getContext('2d');
-	resizeCanvas();
-	requestAnimationFrame(gameLoop);
+  canvas = document.getElementById('stroop') as HTMLCanvasElement;
+  ctx = canvas.getContext('2d');
+  resizeCanvas();
+  let level = loadLevel()
+  tiles = level.tiles
+  player.x = level.playerPosition.col * BLOCK_SIZE
+  player.y = level.playerPosition.row * BLOCK_SIZE
+  requestAnimationFrame(gameLoop);
 }
 
-function drawRect({ x, y, w, h, color }) {
-	ctx.fillStyle = color;
-	ctx.fillRect(x, y, w, h);
+function drawRect(x, y, w, h, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, w, h);
 }
 
-function drawBar({ x, color }) {
-	let y = SCREEN_HEIGHT - 400;
-	drawRect({ x, color, y, w: 20, h: 400 });
+function drawTiles(tiles) {
+  for (var tile of tiles) {
+    drawRect(tile.col * BLOCK_SIZE, tile.row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, tile.color)
+  }
 }
 
 function drawPlayer(player: Player) {
-	let y = SCREEN_HEIGHT - player.height;
-	drawRect({
-		x: player.x,
-		y: y,
-		w: player.width,
-		h: player.height,
-		color: player.color,
-	});
+  let y = SCREEN_HEIGHT - player.height;
+  drawRect(
+    player.x,
+    y,
+    player.width,
+    player.height,
+    player.color,
+  );
 }
 
 type Player = { x; y; width; height; color };
@@ -59,91 +65,80 @@ window.onload = init;
 
 let lastTime = null;
 function gameLoop(timestamp) {
-	if (!lastTime) {
-		lastTime = timestamp;
-		requestAnimationFrame(gameLoop);
-		return;
-	}
-	const dt = timestamp - lastTime;
-	// update player
-	if (pressedKeys.has('right') && !pressedKeys.has('left')) {
-		player.vx = 1;
-	} else if (pressedKeys.has('left') && !pressedKeys.has('right')) {
-		player.vx = -1;
-	} else {
-		player.vx = 0;
-	}
-	player.x += dt / 1000 * player.vx * PLAYER_SPEED;
+  if (!lastTime) {
+    lastTime = timestamp;
+    requestAnimationFrame(gameLoop);
+    return;
+  }
+  const dt = timestamp - lastTime;
+  // update player
+  if (pressedKeys.has('right') && !pressedKeys.has('left')) {
+    player.vx = 1;
+  } else if (pressedKeys.has('left') && !pressedKeys.has('right')) {
+    player.vx = -1;
+  } else {
+    player.vx = 0;
+  }
+  player.x += dt / 1000 * player.vx * PLAYER_SPEED;
 
-	// draw stuff
-	ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  // draw stuff
+  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	drawBar({
-		x: 100,
-		color: 'red',
-	});
-	drawBar({
-		x: 140,
-		color: 'green',
-	});
-	drawBar({
-		x: 180,
-		color: 'blue',
-	});
-	drawPlayer(player);
+  drawTiles(tiles)
+  drawPlayer(player);
 
-	requestAnimationFrame(gameLoop);
-	lastTime = timestamp;
+  requestAnimationFrame(gameLoop);
+  lastTime = timestamp;
 }
 
 const TILE_ID_MAP = {
-	0: 'white',
-	1: 'blue',
-	2: 'green',
-	3: 'red',
-	4: 'gray',
-	5: 'white', // player renders separately. dont worry, he is black
-	6: 'orange',
+  0: 'white',
+  1: 'blue',
+  2: 'green',
+  3: 'red',
+  4: 'gray',
+  5: 'white', // player renders separately. dont worry, he is black
+  6: 'orange',
 };
 
 type Tile = {
-	col: number;
-	row: number;
-	color: string;
+  col: number;
+  row: number;
+  color: string;
 };
 
 type Level = {
-	tiles: Array<Tile>;
-	height: number;
-	width: number;
-	playerPosition: { row: number; col: number };
+  tiles: Array<Tile>;
+  height: number;
+  width: number;
+  playerPosition: { row: number; col: number };
 };
 
 function loadLevel(): Level {
-	const levelJson = require('./level1.json');
-	const properties = levelJson.properties;
-	const rawData = levelJson.layers[0].data;
-	const width: number = levelJson.width;
-	const height: number = levelJson.height;
-	const playerPosition = {
-		col: properties.startPositionCol,
-		row: properties.startPositionRow,
-	};
+  const levelJson = require('./level1.json');
+  const properties = levelJson.properties;
+  const rawData = levelJson.layers[0].data;
+  const width: number = levelJson.width;
+  const height: number = levelJson.height;
+  const playerPosition = {
+    col: properties.startPositionCol,
+    row: properties.startPositionRow,
+  };
 
-	const tiles = rawData.map((tileId, index) => {
-		const col = index % width;
-		const row = Math.floor(index / width);
-		const color = TILE_ID_MAP[tileId];
+  const tiles = rawData.map((tileId, index) => {
+    const col = index % width;
+    const row = Math.floor(index / width);
+    const color = TILE_ID_MAP[tileId];
 
-		return { col, row, color };
-	});
+    return { col, row, color };
+  });
 
-	return {
-		width,
-		height,
-		tiles,
-		playerPosition,
-	};
+  return {
+    width,
+    height,
+    tiles,
+    playerPosition,
+  };
 }
 
 console.error(loadLevel());
